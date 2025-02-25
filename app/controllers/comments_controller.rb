8c6fa47815fcb116eed1b3458421967377880b69
PoulTch <pavelchar@gmail.com>
2025-02-25 09:32:40 +0300

    Minor refactoring

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,12 +6,8 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
     @c = @article.comments.new(comment_params)
     @c.author = current_user.username
-    if @c.save
-      redirect_to article_path(@article)
-    else
-      #render action: 'show'
-      redirect_to article_path(@article)    
-    end
+    @c.save
+    redirect_to article_path(@article)    
   end
 
   private
