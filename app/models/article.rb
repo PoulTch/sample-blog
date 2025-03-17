eb2ce7f62fd854c471daf41ce652330a8d1d1a3c
PoulTch <pavelchar@gmail.com>
2025-03-17 12:54:53 +0300

    Add subject method for Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true
-  validates :text, presence: true 
+  validates :text, presence: true
+
+  def subject
+    title
+  end 
 end
