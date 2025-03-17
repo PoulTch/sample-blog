240bdd35249ee7b01c640296e96bce38605eb484
PoulTch <pavelchar@gmail.com>
2025-03-17 13:02:36 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,4 +9,14 @@ describe Article do
   describe "association" do
     it { should have_many(:comments).dependent(:destroy)  }
   end
+
+  describe "#subject" do
+    it "returns the article title" do
+      # создаем объект article хитрым способом
+      article = create(:article, title: 'Lorem Ipsum')
+
+      # assert, проверка
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
+  end
 end
