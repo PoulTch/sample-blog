c944c61afd0e034b526f6eb28551a4cb2b2393d9
PoulTch <pavelchar@gmail.com>
2025-03-23 11:38:24 +0300

    Add validate of length to Comment model and update comment spec

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :article
-  validates :body, presence: true  
+  validates :body, presence: true
+
+  validates_length_of :body, maximum: 4000  
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,3 +3,7 @@ require "rails_helper"
 describe Comment do
   it { should belong_to(:article)  }
 end
+
+  describe Comment, type: :model do
+    it { should validate_length_of(:body).is_at_most(4000) }
+  end
