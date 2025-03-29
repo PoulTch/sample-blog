7655342fd40f54da81177b1332986a4271eab335
PoulTch <pavelchar@gmail.com>
2025-03-29 14:13:03 +0300

    Fix visitor_creates_account_spec.rb

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 feature "Account Creation" do
   scenario "allows guest to create account" do
-    visit new_user_registration_path
     sign_up
     expect(page).to have_content I18n.t('devise.registrations.signed_up')            
   end
